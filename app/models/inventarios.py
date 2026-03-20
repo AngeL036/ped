c7bb7b69642789251647affbe4311b057bad61a9
Angel Sanchez <angelgabriel036@gmail.com>
2026-03-19 23:58:49 -0600

    acompleatamos los models

--- app/models/inventarios.py
+++ app/models/inventarios.py
@@ -1,16 +1,18 @@
-from sqlalchemy import Column,Integer,String,DateTime, ForeignKey
+from sqlalchemy import Column,Integer,String,DateTime, ForeignKey,Numeric
 from app.database import Base
 from datetime import datetime, timezone
+from sqlalchemy.orm import relationship
 
 class Inventario(Base):
     __tablename__ = "inventario"
 
-    id = Column(Integer, primary_key=True, index=False)
-    producto_id = Column(Integer, ForeignKey("producto.id"))
-    PrecioCompra = Column(Integer,)
-    PrecioVenta = Column()
-    Estado = Column()
+    id               = Column(Integer, primary_key=True, index=False)
+    producto_id      = Column(Integer, ForeignKey("producto.id"), nullable=False)
+    PrecioCompra     = Column(Numeric(10,2), nullable=False)
+    PrecioVenta      = Column(Numeric(10, 2), nullable=False)
+    ganancia         = Column(Numeric(10, 2))
+    valor_inventario = Column(Numeric(10,2))
+    estado           = Column(String(20))
+    create_at        = Column(DateTime(timezone=True),default=lambda: datetime.now(timezone.utc))
 
-    create_at = Column(
-        DateTime(timezone=True),
-        default=lambda: datetime.now(timezone.utc))
+    producto = relationship("Producto", back_populates="inventario")
--- app/models/producto.py
+++ app/models/producto.py
@@ -1,14 +1,21 @@
 from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
-from datetime import datetime
+from datetime import datetime, timezone
 from app.database import Base
-
+from sqlalchemy.orm import relationship
 
 class Producto(Base):
     __tablename__ = "producto"
 
     id             = Column(Integer, primary_key=True, index=False)
+    negocio_id     = Column(Integer, ForeignKey("negocios.id"), nullable=False)
     codigo         = Column(String(30),nullable=True)
-    nombre         = Column(String(40),nullable=False)
-    categoria_id   = Column(Integer,ForeignKey("categorias.id"))
+    nombre         = Column(String(100),nullable=False)
+    categoria_id   = Column(Integer,ForeignKey("categorias.id"), nullable=True)
     unidad         = Column(String(30),nullable=False)
-    cantidadActual = Column(Integer,nullable=False)
+    cantidadActual = Column(Integer,nullable=False, default=0)
+    stock_minimo   = Column(Integer, nullable=False, default=3)
+    created_at     = Column(DateTime(timezone=True), default=lambda : datetime.now(timezone.utc))
+
+    negocio         = relationship("Negocio", back_populates="productos")
+    categoria       = relationship("Categoria", back_populates="productos")
+    inventario      = relationship("Inventario", back_populates="producto")
