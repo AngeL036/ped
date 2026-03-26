35939d882b8005885206e6fdae167eaba2e710ff
Angel Sanchez <angelgabriel036@gmail.com>
2026-03-25 23:03:37 -0600

    mejoramos funcion de venta

--- app/modules/ventas/crud.py
+++ app/modules/ventas/crud.py
@@ -12,41 +12,47 @@ from app.models.producto import Producto
 from app.models.detalleVenta import DetalleVenta
 
 
-def venta(db:Session,negoci_id:int,user:User, venta_in:VentaCreate):
+def venta(db:Session,negocio_id:int,user:User, venta_in:VentaCreate):
     """Registrar una venta"""
-    negocio = db.query(Negocio).filter(Negocio.id == negoci_id).first()
-    if not negocio:
-        raise HTTPException(404,"Negocio no registrado")
+    try:
+        negocio = db.query(Negocio).filter(Negocio.id == negocio_id).first()
+        if not negocio:
+            raise HTTPException(404,"Negocio no registrado")
     
-    nueva_venta = Venta(
-        negocio_id = negoci_id,
-        vendedor = user.id,
-        total = 0
-    )
-    db.add(nueva_venta)
-    db.flush()
-
-    for it in venta_in.items:
-        producto = db.query(Producto).filter(Producto.id == it.producto_id).first()
-        if not producto:
-            raise HTTPException(status_code=404,detail=f"Producto {it.producto_id} no existe")
-        precio = it.cantidad * producto.precio_venta
-        total = total + precio
-        sub_total = it.cantidad * producto.precio_venta
-        detalle_venta = DetalleVenta(
-            venta_id        = nueva_venta.id,
-            producto_id     = it.producto_id,
-            cantidad        = it.cantidad,
-            precio_unitario = producto.precio_venta,
-            sub_total       = sub_total,
+        nueva_venta = Venta(
+            negocio_id = negocio_id,
+            vendedor = user.id,
+            total = 0
         )
-        db.add(detalle_venta)
-    nueva_venta.total = total
-    db.commit()
-    db.refresh()
-    return {
-        "mensaje":"Venta confirmada"
-    }
+        db.add(nueva_venta)
+        db.flush()
+        total = 0
+        for it in venta_in.items:
+            producto = db.query(Producto).filter(Producto.id == it.producto_id).first()
+            if not producto:
+                raise HTTPException(status_code=404,detail=f"Producto {it.producto_id} no existe")
+            if (producto.cantidad_actual < it.cantidad):
+                raise HTTPException(status_code=409, detail="Stock induficiente")
+            sub_total = it.cantidad * producto.precio_venta
+            total += sub_total
+            detalle_venta = DetalleVenta(
+                venta_id        = nueva_venta.id,
+                producto_id     = it.producto_id,
+                cantidad        = it.cantidad,
+                precio_unitario = producto.precio_venta,
+                sub_total       = sub_total,
+            )
+            db.add(detalle_venta)
+        nueva_venta.total = total
+        db.commit()
+        db.refresh(nueva_venta)
+        return {
+            "mensaje":"Venta confirmada"
+        }
+    except Exception as e:
+        db.rollback()
+        print(" ERROR:", e)
+        raise
         
 
 def obtener_mesas_ocupadas(db:Session, negocio_id: int):
